b42fd8d3e111bc4aaf097220d853a4b7710ae9d3
maolivar <63564434+maolivar@users.noreply.github.com>
2021-10-13 22:54:35 -0300

    Major update. - added scenarios with inventory and price discrimination - saves results to local database in Sqlite - Improved template structure and added css - Uses Beleza Natural, valuations.txt

--- main.py
+++ main.py
@@ -7,6 +7,10 @@
 import random
 import base64
 import numpy as np
+import pandas as pd
+import sqlite3 as sql
+import datetime
+
 from io import BytesIO
 
 from flask import Flask, render_template
@@ -17,37 +21,121 @@ from matplotlib.figure import Figure
 
 app = Flask(__name__)
 
-NPERIODS = 3
+FILEVALUATIONS = "valuations.txt"
+DATABASE = 'gameresults.sqlite' # Database file to store results
+GAMEPASSWD = 12345          # Default password to enter the game, also used as game identifier
 
-valdist = list(range(5,200))
-valuations = []
-for n in range(NPERIODS):
-    ncust = random.randint(10,20)
-    valuations.append(random.choices(valdist, k = ncust))
-print(valuations)
+NPERIODS = 5                # Number of weeks in simulation
+NPERIODS_HIGH = 1           # Number of weeks with high valuation in price discrimination setting
 
-@app.route("/")
-def index():
-    global NPERIODS
+#------ DEFINE GAME TYPES --------------------
+GAMETYPES = ['base','inv','disc']  # game types available
+
+# Dictionary specifying if game type has inventory
+HASINV = {'base':False,'inv':True,'disc':True}
+INITINV = 40                # initial inventory for scenarios with inventory
+
+# Dictionary specifiying the distribution on each period for each game type
+GAMEVALUES = {'base':['full']*NPERIODS,
+              'inv':['full']*NPERIODS,
+              'disc':['low']*(NPERIODS-NPERIODS_HIGH)+['high']*NPERIODS_HIGH}
+HIGHVALUE_CUT = 0.2
+NUMCUST_LOW = 10
+NUMCUST_HIGH = 20
+
+# ----------------------------------------------
+
+#----------- GENERATE VALUATIONS FOR EACH GAME TYPE -------------------
+valfile = open(FILEVALUATIONS)
+val_list = valfile.readlines()
+
+valdist = list(map(int, val_list))  # map() applies function int() to each item in val_list
+valdist.sort()
+numobs = len(valdist)
+numcut = int(np.floor(numobs*(1-HIGHVALUE_CUT)))
+
+VALUEDIST = {}
+VALUEDIST['full'] = valdist
+VALUEDIST['high'] = valdist[numcut:(numobs-1)]
+VALUEDIST['low'] = valdist[0:numcut]
+
+VALUATIONS = {}
+
+for g in GAMETYPES:
+    valuations = []
+    for n in range(NPERIODS):
+        valtype = GAMEVALUES[g][n]
+        ncust = random.randint(NUMCUST_LOW,NUMCUST_HIGH)
+        valuations.append(random.choices(VALUEDIST[valtype], k = ncust))
+    VALUATIONS[g] = valuations
+
+
+# CREATE DATABASE
+# con = sql.connect(DATABASE)
+# cur = con.cursor()
+#
+# # Create results table
+# cur.execute("""CREATE TABLE IF NOT EXISTS results
+#                 (timestamp text, gameid text, gametype text, groupid text,
+#                  period integer, price real, ncust integer, sales integer, end_inv integer)""")
+# con.commit()
+# con.close()
+
+
+# ------------ APP FUNCTIONS -------------------------
+
+@app.route('/', defaults={'gametype' : 'base'})
+@app.route("/<string:gametype>")
+def index(gametype):
+    global NPERIODS, VALUATIONS, GAMETYPES, HASINV, GAMEVALUES
+
+    if not gametype in GAMETYPES:
+        return("""<h2> URL not found </h2>""")
+    elif not HASINV[gametype]:
+        init_inv = None
+        inventory = None
+    else:
+        init_inv = INITINV
+        inventory = request.args.get("inv")
+
+    valuations = VALUATIONS[gametype]
+
     price = request.args.get("price")
     stage = request.args.get("stage")
     price_hist = request.args.get("price_hist")
-    sales_hist = request.args.get("sales_hist")
-    if price and stage:
+
+    if init_inv and inventory == None:
+        # Sets inventory to initial inventory in the first stage
+        inventory = init_inv
+
+    if price and stage:     # this is passed after stage 1.
         stagenum = int(stage)
         sales = get_sales(float(price),valuations[stagenum-1])
+        ncust = len(valuations[stagenum-1])
+        salesnum = int(sales)
+
+        if inventory == None:
+            invnum = float("inf")
+        else:
+            invnum = int(inventory)
+
+        salesnum = min(salesnum,invnum)
+        invnum = invnum - salesnum
+        sales = str(salesnum)
+
+        if invnum < float("inf"):
+            inventory = str(invnum)
+
         price_hist = price_hist+price+","
-        sales_hist = sales_hist+sales+","
         stagenum = stagenum + 1
-    else:
+    else:                   # set the first stage
         price_hist=""
-        sales_hist=""
         stagenum = 1
 
     stage = str(stagenum)
 
     if stagenum>1:
-        fig = draw_graph(price_hist)
+        fig = draw_graph(price_hist,init_inv,valuations)
         # Save it to a temporary buffer.
         buf = BytesIO()
         fig.savefig(buf, format="png")
@@ -55,45 +143,78 @@ def index():
         data = base64.b64encode(buf.getbuffer()).decode("ascii")
 
     if stagenum == 1:
-        return render_template('welcome.html', PageTitle= "Welcome",
-                               stage = stage,
+        return render_template('welcome.html', has_inv = HASINV[gametype],
+                               valuetype = GAMEVALUES[gametype][stagenum-1],
+                               value_list = GAMEVALUES[gametype],
+                               stage = stage, inv = inventory,
                                nperiods = str(NPERIODS))
     elif stagenum <= NPERIODS:
-        return render_template('inplay.html',
-                               sales = int(sales), stage=stage, data_plot = data,
-                               price_hist = price_hist, sales_hist = sales_hist)
+        return render_template('base.html', has_inv = HASINV[gametype],
+                               valuetype=GAMEVALUES[gametype][stagenum - 1],
+                               sales = int(sales), ncust = ncust,
+                               stage = stage, data_plot = data,
+                               inv = inventory, init_inv = init_inv,
+                               price_hist = price_hist)
     else:
-        return render_template('gameover.html', sales = int(sales), data_plot = data,
-                               revenue="TO BE CALCULATED")
-        # return (
-        #     head_text + "<br>" +
-        #     "Stage:" + str(stagenum) +
-        #     """<form action="" method="get">
-        #             <input type="hidden" value="%s" name="stage" /> """%str(stagenum) +
-        #     """     <input type="hidden" value="%s" name="sales_hist" /> """%sales_hist +
-        #     """     <input type="hidden" value="%s" name="price_hist" /> """%price_hist +
-        #     """     Choose your price: <input type="text" name="price">
-        #             <input type="submit" value="Submit price">
-        #         </form>"""+
-        #     fig_text
-        # )
+        price_arr = csvstr_to_numarr(price_hist)
+        (sales_arr,ncust_arr)=get_sales_hist(price_arr,init_inv,valuations)
+        totrevenue= np.dot(price_arr,sales_arr)
+        return render_template('gameover.html', has_inv = HASINV[gametype], gametype= gametype,
+                               sales = int(sales), ncust = ncust,
+                               data_plot = data, inv = inventory,
+                               price_hist = price_hist,
+                               cumsales= sum(sales_arr), revenue=totrevenue)
 
-# THIS IS FOR TESTING
-@app.route("/hello")
-def hello():
-    # Generate the figure **without using pyplot**.
-    y = [3,5,6,2,4]
-    x = [1,2,3,4,5]
-    fig = Figure()
-    ax = fig.subplots()
-    ax.plot(x,y)
-    # Save it to a temporary buffer.
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output.
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
+@app.route('/results/', defaults={'gametype' : 'base'})
+@app.route("/results/<string:gametype>")
+def send_results(gametype):
+    global VALUATIONS, GAMETYPES, HASINV
 
+    valuations = VALUATIONS[gametype]
+    if not gametype in GAMETYPES:
+        return("""<h2> URL not found </h2>""")
+    elif HASINV[gametype]:
+        init_inv = INITINV
+    else:
+        init_inv = None
+    price_hist = request.args.get("price_hist")
+    groupid = request.args.get("groupid")
+    if price_hist:
+        currtime = datetime.datetime.now()
+        df = gen_results_table(timestamp=str(currtime), gameid= GAMEPASSWD, gametype= gametype, groupid=groupid,
+                               price_hist= price_hist, init_inv= init_inv, valuations= valuations)
+        try:
+            with sql.connect(DATABASE) as con:
+                df.to_sql('results',con=con, if_exists='append', index= False)
+                con.commit()
+            saved = True
+        except Exception as e:
+            saved = False
+            error_msg = str(e)
+
+        if saved:
+            currgame_index = GAMETYPES.index(gametype)
+            if currgame_index < len(GAMETYPES) - 1:
+                nextgame = GAMETYPES[currgame_index+1]
+            else:
+                nextgame = None
+
+            return render_template("result_confirm.html", tables= [df.to_html(classes='data',header="true")],
+                                   nextgame= nextgame )
+        else:
+            return("""<h1> Error: results could not be saved</h1>"""+error_msg)
+    else:
+        return("""Cannot show results: Invalid price input""")
+
+@app.route('/get_results')
+def retrieve_results():
+    con = sql.connect(DATABASE)
+    df = pd.read_sql('SELECT * FROM results', con=con)
+    con.close()
+    return(df.to_html())
+
+
+# -------------- AUXILIARY FUNCTIONS ------------------------
 
 def get_sales(pricenum,values):
     """Calculate sales for the submitted (numeric) price"""
@@ -102,22 +223,32 @@ def get_sales(pricenum,values):
         sales = sales + (values[i]>=pricenum)
     return str(sales)
 
-def get_sales_hist(price_list):
-    global valuations
+def get_sales_hist(price_list,init_inv,valuations):
+    if init_inv:
+        invnum = int(init_inv)
+    else:
+        invnum = float("inf")
+
     tperiod = len(price_list)
     sales_list = []
     ncust_list = []
     for t in range(tperiod):
-        sales_list.append(int(get_sales(price_list[t],valuations[t])))
+        salesnum = min( int(get_sales(price_list[t],valuations[t])), invnum)
+        invnum = invnum - salesnum
+        sales_list.append(salesnum)
         ncust_list.append(len(valuations[t]))
     sales_arr = np.array(sales_list)
     ncust_arr = np.array(ncust_list)
     return sales_arr, ncust_arr
 
+def csvstr_to_numarr(csv):
+    tolist= [int(x) for x in csv.split(',') if x.strip().isdigit()]
+    return np.array(tolist)
 
-def draw_graph(price_hist):
+def draw_graph(price_hist, init_inv,valuations):
     global NPERIODS
-    price_list=[int(x) for x in price_hist.split(',') if x.strip().isdigit()]
+    XMAX = NPERIODS +2  # length of the x-axis, extended to put label
+    price_list= csvstr_to_numarr(price_hist)
     price_arr = np.array(price_list)
     x = list(range(1,len(price_list)+1))
     fig = Figure()
@@ -125,28 +256,72 @@ def draw_graph(price_hist):
     ax = fig.add_subplot(211)
  #   ax.set_xlabel('week')
     ax.set_ylabel('price')
-    ax.set_xlim(xmin=0, xmax=NPERIODS+1)
+    ax.set_xlim(xmin=0, xmax=XMAX)
     ax.set_xticks(list(range(1,NPERIODS+1)))
     ax.set_title('Price history')
     ax.plot(x, price_list)
     ax.scatter(x, price_list)
 
-    (sales_arr,ncust_arr) = get_sales_hist(price_list)
+    (sales_arr,ncust_arr) = get_sales_hist(price_list, init_inv,valuations)
     lostsales_arr = ncust_arr - sales_arr
     ax2 = fig.add_subplot(212)
     ax2.bar(x, sales_arr, label='Sales')
     ax2.bar(x, lostsales_arr, bottom=sales_arr, label="No purchase")
     ax2.set_xlabel('week')
     ax2.set_ylabel('customers')
-    ax2.set_xlim(xmin=0, xmax=NPERIODS+1)
+    ax2.legend(loc='center right')
+    ax2.set_xlim(xmin=0, xmax=XMAX)
     ax2.set_xticks(list(range(1,NPERIODS+1)))
     ax2.set_title('Sales history')
 
+    fig.tight_layout()
+
     return fig
+
+def gen_results_table(timestamp, gameid, gametype, groupid, price_hist, init_inv,valuations):
+    """ Calculates results table from price_hist string"""
+    price = csvstr_to_numarr(price_hist)
+    (sales,ncust) = get_sales_hist(price, init_inv,valuations)
+    cumsales = np.cumsum(sales)
+    nperiods = len(price)
+    period = np.array(range(1,nperiods+1))
+    if init_inv:
+        inventory = np.repeat(int(init_inv),nperiods) - cumsales
+    else:
+        inventory = np.repeat(None,nperiods)
+
+    data = {'timestamp':timestamp,
+             'gameid':gameid,
+             'gametype':gametype,
+            'groupid': groupid,
+            'period':period,
+            'price':price,
+            'ncust':ncust,
+            'sales':sales,
+            'end_inv':inventory}
+
+    df = pd.DataFrame(data)
+    return df
+
+
+
+
+
+
 
 # Following line is to run locally
 #if __name__ == "__main__":
 #    app.run(host="127.0.0.1", port=8080, debug=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # Use the following when deploying with Procfile using gunicorn
+    # See: https: // dev.to / lordofdexterity / deploying - flask - app - on - heroku - using - github - 50
+    # Procfile >> web: gunicorn --bind 0.0.0.0:$PORT main:app (This is the Procfile)
+    app.run(debug=True) # final deployment set debug=False
+
+    # The following can be used when Procfile uses python directly:
+    # (see https://www.youtube.com/watch?v=OUXzdPnh6wI )
+    # Procfile >> web: python main.py (this is the Procfile)
+    # Use the following to run with the "python" Procfile.
+    # port = os.environ.get("PORT",5000) # Requires the os library.
+    # app.run(debug=True,host="0.0.0.0,port=port)
